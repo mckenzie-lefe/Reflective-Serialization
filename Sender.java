0f01f6ff66fdaef90801e2aac31d8455f49dbcd3
McKenzie Lefebvre <109600778+mckenzie-lefe@users.noreply.github.com>
2023-11-02 21:22:55 -0600

    V9 - Refactor Sender
    - extract method serializeObjects
    - rename variables
    - bug fixes
    - format output

--- Sender.java
+++ Sender.java
@@ -4,73 +4,40 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.*;
 
-import org.jdom2.Document;
+import org.jdom2.*;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
 public class Sender {   
     private static Scanner scanner = new Scanner(System.in);
-/* 
-    private static String getFilePath() {
-        System.out.print("Enter XML file path for JDOM document: ");
-        String f = scanner.nextLine();
 
-        if (!f.endsWith(".xml")) {
-            System.out.println("Invalid file path. Using default " + defaultXMLFilePath);
-            return defaultXMLFilePath;
-        }
-        return f;
-    }
-
-    private static String getServerHost() {
-        System.out.print("Enter server hostname of the Receiver program: ");
-        String host = scanner.nextLine();
-
-        if (!host.matches("(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])")) {
-            System.out.println("Invalid hostname. Using default " + defaultServerHost);
-            return defaultServerHost;
-        }
-        return host;
-    }
-
-    private static int getServerPort() {
-        System.out.print("Enter server port of the Receiver program: ");
-        String port = scanner.nextLine();
-
-        try {
-           return Integer.parseInt(port);
-        } catch (NumberFormatException e) {
-             System.out.println("Invalid hostname. Using default " + defaultServerPort);
-            return defaultServerPort;
-        }
-    }
-*/
     private static Map<String, String> changeSettings(Map<String, String> settings) {
 
         System.out.print("Enter XML file path for JDOM document: ");
-        String f = scanner.nextLine();
+        String f = scanner.next();
 
         if (!f.endsWith(".xml")) 
-            System.out.println("Invalid file path. Using " + settings.get("XMLFilePath"));
+            System.out.println("WARNING: Invalid file path. Using " + settings.get("XMLFilePath"));
         else 
             settings.put("XMLFilePath", f);
 
         System.out.print("Enter server hostname of the Receiver program: ");
-        String host = scanner.nextLine();
+        String host = scanner.next();
 
         if (!host.matches("(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9])\\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\\-]*[A-Za-z0-9])")) 
-            System.out.println("Invalid hostname. Using " + settings.get("hostname"));
+            System.out.println("WARNING: Invalid hostname. Using " + settings.get("hostname"));
         else 
             settings.put("hostname", host);
 
         System.out.print("Enter server port of the Receiver program: ");
-        String portStr = scanner.nextLine();
+        String portStr = scanner.next();
 
         try {
            Integer.parseInt(portStr);
-           settings.put(host, portStr);
+           settings.put("port", portStr);
+
         } catch (NumberFormatException e) {
-             System.out.println("Invalid hostname. Using " + settings.get("port"));
+             System.out.println("WARNING: Invalid hostname. Using " + settings.get("port"));
         }
 
         return settings;
@@ -78,7 +45,7 @@ public class Sender {
 
     private static void displayMenu(Map<String, String> settings) {
         System.out.println( 
-            "The Sender program:\n" +//
+            "\nThe Sender program:\n" +//
             " - create object(s) using Object Creation Menu," + // 
             " - serialize these objects into a JDOM document, and\n" + // 
             " - send this document to the Receiver program over network\n\n" + //
@@ -108,11 +75,6 @@ public class Sender {
         settings.put("XMLFilePath", "serialized.xml");
         settings.put("hostname", "localhost");
         settings.put("port", "4000");
-        Document doc;
-        List<Object> objects;
-        String outXMLFilePath;
-        String serverHost;
-        int serverPort;
 
         while(true)
         {
@@ -122,84 +84,59 @@ public class Sender {
             switch (choice) {
                 case 1:
                     changeSettings(settings);
-                    for (Map.Entry<String, String> e: settings.entrySet())
-                    {
-                        System.out.println(e.getValue());
-                    }
                     break;
 
                 case 2:
-                    objects = ObjectCreator.createObjects();
-                    System.out.println("Serializing objects... (to stdout and in file " + settings.get("XMLFilePath")+ ")");
-                    doc = new Serializer().serialize(objects);
-                    try {
-                        XMLOutputter outputXML = new XMLOutputter();
-                        outputXML.setFormat(Format.getPrettyFormat());
-                        outputXML.output(doc, new FileWriter(settings.get("XMLFilePath")));
-
-                        // print file to console
-                        BufferedReader in = new BufferedReader(new FileReader(settings.get("XMLFilePath")));
-                        in.lines().forEach(line -> { System.out.println(line); });
-                        in.close();
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    sendObject(doc, settings.get("hostname"), Integer.parseInt(settings.get("port")));
+                    sendObject(
+                        serialzeObjects(ObjectCreator.createObjects(), settings.get("XMLFilePath")), 
+                        settings.get("hostname"), 
+                        Integer.parseInt(settings.get("port"))
+                    );
                     break;
             
                 default:
                    return;
             }
-            //objects = ObjectCreator.createObjects();
-            //System.out.println(String.format("%-80s", "").replace(' ', '='));
-            //outXMLFilePath = getFilePath();
-            //System.out.println("Serializing objects... (to stdout and in file " + outXMLFilePath + ")");
-            //System.out.println(String.format("%-80s", "").replace(' ', '='));
-            //doc = new Serializer().serialize(objects);
-/* 
-            try
-            {
-                XMLOutputter outputXML = new XMLOutputter();
-                outputXML.setFormat(Format.getPrettyFormat());
-                outputXML.output(doc, new FileWriter(outXMLFilePath));
-
-                // print file to console
-                BufferedReader in = new BufferedReader(new FileReader(outXMLFilePath));
-                in.lines().forEach(line -> { System.out.println(line); });
-                in.close();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-*/
-            //System.out.println(String.format("%-80s", "").replace(' ', '='));
-            //sendObject(doc, getServerHost(), getServerPort());
-            //System.out.println(String.format("%-80s", "").replace(' ', '='));
-            //System.out.println("Exit program? (y/n)");
-            //if(scanner.nextLine().contains("y"))
-            //    break;
-            //System.out.println(String.format("%-80s", "").replace(' ', '='));
         }
     }
+
+    private static Document serialzeObjects(List<Object> objects, String file) {
+        System.out.println("Serializing objects... (to stdout and in file " +file+ ")");
+        Document doc = new Serializer().serialize(objects);
+        try {
+            XMLOutputter outputXML = new XMLOutputter();
+            outputXML.setFormat(Format.getPrettyFormat());
+            outputXML.output(doc, new FileWriter(file));
+
+            // print file to console
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            in.lines().forEach(line -> { System.out.println(line); });
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return doc;
+    }
     
-    private static void sendObject(Object obj, String hostname, int port)
+    private static void sendObject(Document obj, String hostname, int port)
     {
-        System.out.println("Sending serialized object to " + hostname + " at port " + port);
+        System.out.println("Sending serialized objects to " + hostname + ":" + port);
         try
         {
+            System.out.println("Connecting...");
             Socket socket = new Socket(InetAddress.getByName(hostname), port);
-            System.out.println("Socket connected at " + hostname + ":" + port);
             ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            System.out.println("Connected to output stream");
+            System.out.println("Sending ... ");
+
             out.writeObject(obj);
             out.flush();
-            System.out.println("Object written to output stream");  
+            System.out.println("Sent.");  
+
             socket.close();
-            System.out.println("Socket closed");
 
         } catch (ConnectException e) {
-            System.out.println("Unable to connect to socket.");
+            System.out.println("WARNING: Unable to make socket connection.");
         } catch (Exception e) {
             e.printStackTrace();
         }
